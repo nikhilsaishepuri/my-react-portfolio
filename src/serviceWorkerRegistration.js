ce61e6cbefbfb10d8e484ffa796960d408bfac5f
Nikhil sai <nikhilsaishepuri@gmail.com>
2026-03-24 16:11:01 +0530

    fix PWA install

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,32 +1,14 @@
-const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "[::1]" ||
-  window.location.hostname.match(
-    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
-  ),
-);
-
 export function register() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      const swUrl = "/service-worker.js";
-
       navigator.serviceWorker
-        .register(swUrl)
+        .register("/service-worker.js")
         .then((registration) => {
-          console.log("Service Worker registered:", registration);
+          console.log("SW registered: ", registration);
         })
         .catch((error) => {
-          console.log("Service Worker registration failed:", error);
+          console.log("SW registration failed: ", error);
         });
-    });
-  }
-}
-
-export function unregister() {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready.then((registration) => {
-      registration.unregister();
     });
   }
 }
